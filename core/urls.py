fd6e7e8b90d43d67b7ff75fbca474fa2516782a9
480pDreams <mrmetalgear91@gmail.com>
2025-11-30 15:48:42 -0500

    bug fix

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('hardware/', views.hardware, name='hardware'),
     path('api/update-theme/', views.update_theme, name='update_theme'),
 ]
