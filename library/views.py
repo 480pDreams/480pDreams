18933586c4a23edfbf9dfdc5f1d3e92ba8cad6ed
480pDreams <mrmetalgear91@gmail.com>
2025-11-30 14:14:10 -0500

    new

--- library/views.py
+++ library/views.py
@@ -4,14 +4,20 @@ from .filters import GameFilter
 
 
 def game_list(request):
+    # 1. Get Base Queryset
     all_games = Game.objects.all().order_by('title')
+
+    # 2. Apply Filter
     my_filter = GameFilter(request.GET, queryset=all_games)
     games = my_filter.qs
+
+    # 3. Get Platforms (For the icon bar)
     platforms = Platform.objects.all().order_by('name')
+
     context = {
         'games': games,
         'filter': my_filter,
-        'platforms': platforms,
+        'platforms': platforms,  # <--- Crucial: Pass this to the template!
     }
     return render(request, 'library/game_list.html', context)
 
