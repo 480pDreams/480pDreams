9b9278e1eab3cce5cc97b1e03a68920777c18f86
480pDreams <mrmetalgear91@gmail.com>
2025-12-01 17:43:06 -0500

    Fix hardware image display and filter logic

--- hardware/filters.py
+++ hardware/filters.py
@@ -1,6 +1,5 @@
 import django_filters
 from django import forms
-from django.db.models import Q
 from .models import Hardware, HardwareType
 from library.models import Platform, Region
 
@@ -9,32 +8,30 @@ class HardwareFilter(django_filters.FilterSet):
     # 1. SEARCH
     name = django_filters.CharFilter(
         lookup_expr='icontains',
-        label='Search Name',
-        widget=forms.TextInput(attrs={'placeholder': 'Enter name...'})
+        label='Search Name'
     )
 
-    # 2. DROPDOWNS / CHIPS (Multi-Select)
-    type = django_filters.ModelMultipleChoiceFilter(queryset=HardwareType.objects.all())
-    platform = django_filters.ModelMultipleChoiceFilter(queryset=Platform.objects.all())
-    regions = django_filters.ModelMultipleChoiceFilter(queryset=Region.objects.all())
-
-    # 3. DATE RANGE (Calendar Widgets)
-    release_date_min = django_filters.DateFilter(
-        field_name='release_date',
-        lookup_expr='gte',
-        widget=forms.DateInput(attrs={'type': 'date'})
+    # 2. DROPDOWNS (Set conjoined=False to handle empty/OR logic correctly)
+    type = django_filters.ModelMultipleChoiceFilter(
+        queryset=HardwareType.objects.all(),
+        conjoined=False
     )
-    release_date_max = django_filters.DateFilter(
-        field_name='release_date',
-        lookup_expr='lte',
-        widget=forms.DateInput(attrs={'type': 'date'})
+    platform = django_filters.ModelMultipleChoiceFilter(
+        queryset=Platform.objects.all(),
+        conjoined=False
+    )
+    regions = django_filters.ModelMultipleChoiceFilter(
+        queryset=Region.objects.all(),
+        conjoined=False
     )
 
-    # 4. BOOLEANS (Checkboxes)
+    # 3. DATE RANGE
+    release_date_min = django_filters.DateFilter(field_name='release_date', lookup_expr='gte')
+    release_date_max = django_filters.DateFilter(field_name='release_date', lookup_expr='lte')
+
+    # 4. BOOLEANS
     own_item = django_filters.BooleanFilter(method='filter_own_item', widget=forms.CheckboxInput)
     missing_box = django_filters.BooleanFilter(method='filter_missing_box', widget=forms.CheckboxInput)
-
-    # Content Checks
     has_review = django_filters.BooleanFilter(method='filter_has_review', widget=forms.CheckboxInput)
     has_unboxing = django_filters.BooleanFilter(field_name='video_condition', lookup_expr='isnull', exclude=True,
                                                 widget=forms.CheckboxInput)
@@ -51,14 +48,13 @@ class HardwareFilter(django_filters.FilterSet):
             'release_date': 'Release Date',
             'created_at': 'Date Added',
         },
-        label='Sort By'
     )
 
     class Meta:
         model = Hardware
         fields = []
 
-    # --- LOGIC ---
+    # LOGIC
     def filter_own_item(self, queryset, name, value):
         return queryset.filter(own_item=True) if value else queryset
 
